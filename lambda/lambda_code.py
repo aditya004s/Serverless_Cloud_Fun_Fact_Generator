2f407246ea375ba21963123306223107d4ce3a46
Aditya Singh <140856851+aditya004s@users.noreply.github.com>
2026-06-15 23:04:15 +0530

    Add Lambda function to retrieve random facts

---
+++ lambda/lambda_code.py
@@ -0,0 +1,19 @@
+import boto3
+import random
+import json
+dynamodb=boto3.resource("dynamodb")
+table=dynamodb.Table("fact_generator")
+def lambda_handler(event, context):
+    response=table.scan()
+    items=response.get("Items", [])
+    if not items:
+        return{ 
+            "statusCode":500, 
+            "body":json.dumps({"error":"No facts found"}) 
+        }
+    fact=random.choice(items)["Fact"]
+    return{ 
+        "statusCode":200, 
+        "headers":{"Content-Type":"application/json"}, 
+        "body":json.dumps({"fact":fact}) 
+    }
